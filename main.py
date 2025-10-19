f2b31ad68d6f2d5c40d78c0f0c4a7eb2d6a586d6
TuckerNielson <tuck.13@yahoo.com>
2025-10-19 11:46:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
